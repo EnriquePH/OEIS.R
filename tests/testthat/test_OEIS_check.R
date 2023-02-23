0a13a6fecb2d2f262a15e2c3f2a56b4e184a9537
EPH-KAGGLE@bitbucket.org <eph.project1500@gmail.com>
2023-02-23 21:39:16 +0100

    reorder id

--- tests/testthat/test_OEIS_check.R
+++ tests/testthat/test_OEIS_check.R
@@ -1,10 +1,9 @@
 id_error <- "A002000Z"
+id_ok <- "A002000"
 
 test_that("OEIS_check shows an error", {
   testthat::expect_error(OEIS_check(id_error))
 })
-
-id_ok <- "A002000"
 
 test_that("OEIS_check works", {
   testthat::expect_error(OEIS_check(id_ok), NA)
--- tests/testthat/test_OEIS_url.R
+++ tests/testthat/test_OEIS_url.R
@@ -1,10 +1,9 @@
 id_error <- "A00005"
+id_ok <- "A000055"
 
 test_that("OEIS_url shows an error", {
   testthat::expect_error(OEIS_url(id_error))
 })
-
-id_ok <- "A000055"
 
 test_that("OEIS_url returns and object of class character", {
   testthat::expect_equal(class(OEIS_url(id_ok)), "character")
